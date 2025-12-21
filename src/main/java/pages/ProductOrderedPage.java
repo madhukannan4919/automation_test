bf436ae34c1b2d3c5e628ebbe698dc07e7dea86a
Madhu kannan <madhukannan@Madhus-MacBook-Air.local>
2025-12-21 20:13:23 +0530

    check

--- src/main/java/pages/ProductOrderedPage.java
+++ src/main/java/pages/ProductOrderedPage.java
@@ -17,6 +17,9 @@ public class ProductOrderedPage extends BasePage {
     }
 
 //   i am changed this code
+    //gopal
+    //boppal
+    //kjsdioow
     private By homepageLogo = By.xpath("//div[@class='logo pull-left']");
     private By btnProduct = By.xpath("//a[@href='/products']");
     private By searchBoxProduct = By.id("search_product");
