ae541bea4b18dc135befe5b806c5bea16bf87ba7
madhukannan4919 <madhukannan4919@gmail.com>
2025-12-25 17:13:04 +0530

    changes

--- src/main/java/pages/ScrollFunctionPage.java
+++ src/main/java/pages/ScrollFunctionPage.java
@@ -1,9 +1,12 @@
 package pages;
 
 import base.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ScrollFunctionPage extends BasePage {
 
     public ScrollFunctionPage(WebDriver driver){super((driver));}
+
+    private By homepageLogo = By.xpath("//div[@class='logo pull-left']");
 }
